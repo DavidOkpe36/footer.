aa51954fb92565e1516a9e37c1fc6a6f70d2b666
David Okpe <106525012+DavidOkpe36@users.noreply.github.com>
2022-08-02 00:55:08 +0100

    Initial commit

---
+++ js/nav.js
@@ -0,0 +1,26 @@
+//we created a navbar across all pages//
+const createNav = function () {
+    let nav = document.querySelector('.navbar');
+
+    nav.innerHTML = ` <div class="nav">
+    <img src="img/dark-logo.png" class="brand-logo" alt="">
+
+<div class="nav-items">
+    <div class="search">
+        <input type="text" class="search-box" placeholder="search brand, products">
+        <button class="search-button">Button</button>
+        <a href="#"><img src="img/user.png" alt=""></a>
+        <a href="#"><img src="img/cart.png" alt=""></a>
+    </div>
+</div>
+</div>
+<ul class="links-container">
+<li class="link-item"><a href="#" class="link">home</a></li>
+<li class="link-item"><a href="#" class="link">women</a></li>
+<li class="link-item"><a href="#" class="link">men</a></li>
+<li class="link-item"><a href="#" class="link">kids</a></li>
+<li class="link-item"><a href="#" class="link">accessories</a></li>
+</ul>`
+};
+
+createNav();
